2b51c1d447d818c0473e0db42cd56a59975efa53
b.glimm <b.glimm@e1f49dc5-4708-6b8c-a369-f9a0ab865bed>
2010-07-19 10:22:15 +0000

    all model objects are now immutable

---
+++ trunk/src/org/semanticweb/sparql/owlbgp/model/properties/AnnotationPropertyVariable.java
@@ -0,0 +1,68 @@
+/* Copyright 2010 by the Oxford University Computing Laboratory
+   
+   This file is part of OWL-BGP.
+
+   OWL-BGP is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   OWL-BGP is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public License
+   along with OWL-BGP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.semanticweb.sparql.owlbgp.model.properties;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.sparql.owlbgp.model.Atomic;
+import org.semanticweb.sparql.owlbgp.model.ExtendedOWLObject;
+import org.semanticweb.sparql.owlbgp.model.ExtendedOWLObjectVisitorEx;
+import org.semanticweb.sparql.owlbgp.model.InterningManager;
+import org.semanticweb.sparql.owlbgp.model.OWLAPIConverter;
+import org.semanticweb.sparql.owlbgp.model.Variable;
+
+public class AnnotationPropertyVariable extends Variable implements AnnotationPropertyExpression,Atomic {
+    private static final long serialVersionUID = -971279445618480330L;
+    
+    protected static InterningManager<AnnotationPropertyVariable> s_interningManager=new InterningManager<AnnotationPropertyVariable>() {
+        protected boolean equal(AnnotationPropertyVariable object1,AnnotationPropertyVariable object2) {
+            return object1.m_variable==object2.m_variable;
+        }
+        protected int getHashCode(AnnotationPropertyVariable object) {
+            int hashCode=71;
+            hashCode+=object.m_variable.hashCode();
+            return hashCode;
+        }
+    };
+    protected AnnotationPropertyVariable(String variable) {
+        super(variable);
+    }
+    public ExtendedOWLObject getBoundVersion(Atomic binding) {
+        if (binding==null || binding instanceof AnnotationProperty) return binding;
+        else throw new RuntimeException("Error: Only annotation properties can be assigned to annotation property variables, but annotation property variable "+m_variable+" was assigned the non-annotation property "+binding);
+    }
+    protected Object readResolve() {
+        return s_interningManager.intern(this);
+    }
+    public static AnnotationPropertyVariable create(String variable) {
+        return s_interningManager.intern(new AnnotationPropertyVariable(variable));
+    }
+    public <O> O accept(ExtendedOWLObjectVisitorEx<O> visitor) {
+        return visitor.visit(this);
+    }
+    protected OWLObject convertToOWLAPIObject(OWLAPIConverter converter) {
+        return converter.visit(this);
+    }
+    public Set<Variable> getVariablesInSignature(VarType varType) {
+        Set<Variable> variables=new HashSet<Variable>();
+        if (varType==null||varType==VarType.ANNOTATION_PROPERTY) variables.add(this);
+        return variables;
+    }
+}
---
+++ trunk/src/org/semanticweb/sparql/owlbgp/model/properties/DataPropertyVariable.java
@@ -0,0 +1,69 @@
+/* Copyright 2010 by the Oxford University Computing Laboratory
+   
+   This file is part of OWL-BGP.
+
+   OWL-BGP is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   OWL-BGP is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+   
+   You should have received a copy of the GNU Lesser General Public License
+   along with OWL-BGP.  If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.semanticweb.sparql.owlbgp.model.properties;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.model.OWLObject;
+import org.semanticweb.sparql.owlbgp.model.Atomic;
+import org.semanticweb.sparql.owlbgp.model.ExtendedOWLObject;
+import org.semanticweb.sparql.owlbgp.model.ExtendedOWLObjectVisitorEx;
+import org.semanticweb.sparql.owlbgp.model.InterningManager;
+import org.semanticweb.sparql.owlbgp.model.OWLAPIConverter;
+import org.semanticweb.sparql.owlbgp.model.Variable;
+
+public class DataPropertyVariable extends Variable implements DataPropertyExpression {
+    private static final long serialVersionUID = 3644196667162641608L;
+
+    protected static InterningManager<DataPropertyVariable> s_interningManager=new InterningManager<DataPropertyVariable>() {
+        protected boolean equal(DataPropertyVariable object1,DataPropertyVariable object2) {
+            return object1.m_variable==object2.m_variable;
+        }
+        protected int getHashCode(DataPropertyVariable object) {
+            int hashCode=43;
+            hashCode+=object.m_variable.hashCode();
+            return hashCode;
+        }
+    };
+    
+    protected DataPropertyVariable(String variable) {
+        super(variable);
+    }
+    public ExtendedOWLObject getBoundVersion(Atomic binding) {
+        if (binding instanceof DataProperty || binding==null) return binding;  
+        else throw new RuntimeException("Error: Only data properties can be assigned to data property variables, but data proiperty variable "+m_variable+" was assigned the non-data property "+binding);
+    }
+    protected Object readResolve() {
+        return s_interningManager.intern(this);
+    }
+    public static DataPropertyVariable create(String variable) {
+        return s_interningManager.intern(new DataPropertyVariable(variable));
+    }
+    public <O> O accept(ExtendedOWLObjectVisitorEx<O> visitor) {
+        return visitor.visit(this);
+    }
+    protected OWLObject convertToOWLAPIObject(OWLAPIConverter converter) {
+        return converter.visit(this);
+    }
+    public Set<Variable> getVariablesInSignature(VarType varType) {
+        Set<Variable> variables=new HashSet<Variable>();
+        if (varType==null||varType==VarType.DATA_PROPERTY) variables.add(this);
+        return variables;
+    }
+}
